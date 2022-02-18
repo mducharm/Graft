abb695c23155585fe935c283760f19cde2e86183
Michael Ducharm <ducharmdev@gmail.com>
2022-02-18 17:10:04 -0500

    typo

--- PlotNet/Models/GraphDTO.cs
+++ PlotNet/Models/GraphDTO.cs
@@ -3,7 +3,7 @@ namespace PlotNet.Models;
 
 public record GraphDTO(
     IEnumerable<Node> Nodes, 
-    IEnumerable<Edge> Edgess
+    IEnumerable<Edge> Edges
 );
 
 public record Node(string Id, Dictionary<string, string> Data);
