a4825308306673f4bd6fd214ffb16dfce765ebf5
Michael Ducharm <ducharmdev@gmail.com>
2022-02-22 18:38:37 -0500

    extract out navbar

---
+++ PlotNet.UI/src/lib/NavBar.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import logo from "../assets/PlotNet-logos_transparent.png";
+</script>
+
+<nav>
+  <div class="nav-container">
+    <img src={logo} alt="Svelte Logo" />
+  </div>
+</nav>
+
+<style>
+  nav {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    background-color: #efefef;
+    overflow: visible;
+    height: 6rem;
+  }
+
+  .nav-container {
+    max-width: 1460px;
+  }
+
+  img {
+    height: 7rem;
+    width: 7rem;
+  }
+</style>
